aa93aeda70891d88d1a150f74856f616d65f214f
rymph <rymph0501@gmail.com>
2024-10-09 20:26:30 +0900

    [BOJ] 칸토어 집합 / 실버3 / 15분
    https://www.acmicpc.net/problem/4779

---
+++ rymph/BOJ/Recursion/BOJ_4779.java
@@ -0,0 +1,34 @@
+package BOJ.Recursion;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+// 20:00 시작
+public class BOJ_4779 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = "";
+        while((input = br.readLine()) != null){
+            int N = Integer.parseInt(input);
+            bw.write(cantor(N) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+    public static String cantor(int n){
+        // 3**n 개의 -가 있을 때,
+        if(n == 0){
+            return "-";
+        }else{
+            StringBuilder sb = new StringBuilder();
+            return sb.append(cantor(n-1)).append(" ".repeat((int) Math.pow(3, n-1))).append(cantor(n-1)).toString();
+        }
+    }
+}
