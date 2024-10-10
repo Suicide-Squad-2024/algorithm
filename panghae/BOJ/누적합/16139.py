bf6518b79e1b518f358d51a7428ed3737b9de7b1
PangHae <yds05074@gmail.com>
2024-10-11 01:23:55 +0900

    [BOJ] 인간-컴퓨터 상호작용 / 주석 추가

--- panghae/BOJ/누적합/16139.py
+++ panghae/BOJ/누적합/16139.py
@@ -23,3 +23,32 @@ for _ in range(N):
     index = ord(word) - 97
 
     sys.stdout.write(f'{prefix_sum[index][last+1] - prefix_sum[index][start]}\n')
+
+# import sys
+# from collections import defaultdict
+#
+# read = sys.stdin.readline
+#
+# string_value = read().rstrip()
+#
+# N = int(read().rstrip())
+#
+# prefix_sum_tree = defaultdict(list)
+#
+# word_count = [0] * 26
+#
+# for i in range(26):
+#     prefix_sum_tree[chr(i + 97)].append(0)
+#
+# for string in string_value:
+#     word_count[ord(string) - 97] += 1
+#     for i in range(26):
+#         prefix_sum_tree[chr(i + 97)].append(word_count[i])
+#
+# for _ in range(N):
+#     word, s_start, s_last = read().rstrip().split()
+#
+#     start = int(s_start) + 1
+#     last = int(s_last) + 1
+#
+#     sys.stdout.write(f'{prefix_sum_tree[word][last] - prefix_sum_tree[word][start - 1]}\n')
