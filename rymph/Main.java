5b6d4e5b75c84079d39875dfaecd01995104032e
rymph <rymph0501@gmail.com>
2024-10-26 20:32:32 +0900

    [BOJ] 동전 0 / 실버4 / 5분
    https://www.acmicpc.net/problem/11047

--- rymph/Main.java
+++ rymph/Main.java
@@ -1,35 +1,27 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
-    static int[][] dp;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        dp = new int[14][14];
-        int init = 1;
-        for(int i = 0; i < 14; i++){
-            dp[0][i] = init;
-            init += (i+2);
-        }
-        for(int i = 0; i < 14; i++){
-            dp[i][0] = 1;
-        }
-
-        for(int i = 1; i < 14; i++){
-            for(int j = 1; j < 14; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        for(int i = 0; i < 3; i++){
+            int value;
+            try{
+                value = Integer.parseInt(br.readLine());
+            }catch(NumberFormatException e){
+                continue;
             }
-        }
-        for(int i = 0; i < T; i++){
-            int K = Integer.parseInt(br.readLine());
-            int N = Integer.parseInt(br.readLine());
-            bw.write(dp[K-1][N-1] + "\n");
-        }
 
-        bw.flush();
-        bw.close();
+            int test = value + (3 - i);
+            if(test % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(test % 3 == 0){
+                System.out.println("Fizz");
+            }else if(test % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(test);
+            }
+            break;
+        }
     }
-
 }
