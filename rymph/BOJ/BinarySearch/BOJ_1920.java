6b3ec55e9fe87e931ae18c366231de38e2274c89
rymph <rymph0501@gmail.com>
2024-10-30 02:13:28 +0900

    [BOJ] 수 찾기 / 실버4 / 5분
    https://www.acmicpc.net/problem/1920

---
+++ rymph/BOJ/BinarySearch/BOJ_1920.java
@@ -0,0 +1,33 @@
+package BOJ.BinarySearch;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Set;
+import java.util.HashSet;
+
+public class BOJ_1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Set<Integer> set = new HashSet<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        br.close();
+        for (int i = 0; i < M; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            if (set.contains(value)) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
